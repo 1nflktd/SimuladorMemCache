ff2b201ac9b7804c2d72a4b4ef865a62f6747c7a
1nflktd <henscalon@hotmail.com>
2015-05-24 21:39:00 -0300

    Update mem_cache.cpp

--- mem_cache.cpp
+++ mem_cache.cpp
@@ -16,7 +16,7 @@ int main()
 	int numeroBlocosConjunto;
 	std::cout << "Número de blocos por conjunto: ";
 	std::cin >> numeroBlocosConjunto;
-	/*
+
 	int politicaEscrita;
 	std::cout << "Politica de escrita (1 - write-back, 0 - write-through): ";
 	std::cin >> politicaEscrita;
@@ -36,43 +36,80 @@ int main()
 	int politicaSubstituicao;
 	std::cout << "Politica de substituicao (0 - LRU , 1 - aleatoria): ";
 	std::cin >> politicaSubstituicao;
-	*/
 
 	int tamanhoCache = tamanhoBloco * numeroBlocos;
 	int numeroConjuntos = tamanhoCache / (numeroBlocosConjunto * tamanhoBloco);
 
 	std::vector<std::vector<int>> cache(numeroConjuntos, std::vector<int>(numeroBlocosConjunto));
 
-	int endereco, verificacoes = 0, acertos = 0, erros = 0;
+	int endereco, verificacoes = 0, acertos = 0, erros = 0, enderecosEscrita = 0,
+		leiturasMemoria = 0, escritasMemoria = 0;
 	char tipoLeituraEscrita;
 
 	std::ifstream arq("teste.cache");
 	while (arq >> std::hex >> endereco >> tipoLeituraEscrita)
 	{
+		if (tipoLeituraEscrita == 'W')
+			enderecosEscrita++;
 		verificacoes++;
 		int conjunto = (endereco / tamanhoBloco) % numeroConjuntos;
 		int rotulo = endereco / (tamanhoBloco * numeroConjuntos);
 		size_t i = 0;
 		while (i < numeroBlocosConjunto && cache[conjunto][i] != rotulo)
 			i++;
-		
+
 		if (i < numeroBlocosConjunto && cache[conjunto][i] == rotulo)
 		{
 			acertos++;
+			if (tipoLeituraEscrita == 'W')
+			{
+				if (politicaEscrita == 0) // write through
+				{
+					// escreve na memoria
+					escritasMemoria++;
+				}
+				else // write back
+				{
+
+				}
+			}
 		}
 		else
 		{
 			erros++;
 			int aleatorio = rand() % numeroBlocosConjunto;
 			cache[conjunto][aleatorio] = rotulo;
+			// total de leituras da memoria
+			leiturasMemoria++;
 		}
 	}
 
-	std::cout 
-		<< "Numero de verificacoes: " << verificacoes << std::endl
-		<< "Numero de acertos: " << acertos << std::endl
-		<< "Numero de erros: " << erros << std::endl
-		;
+	std::ofstream res("res.txt");
+	res
+		<< "Parametros de entrada: " << std::endl
+		<< "Tamanho do bloco " << tamanhoBloco << std::endl
+		<< "Numero de blocos " << numeroBlocos << std::endl
+		<< "Blocos por conjunto " << numeroBlocosConjunto << std::endl
+		<< "Politica de escrita " << politicaEscrita << std::endl
+		<< "Tempo de leitura (em ns) " << tempoLeitura << std::endl
+		<< "Tempo de escrita (em ns) " << tempoEscrita << std::endl
+		<< "Tempo de acesso (em ns) " << tempoAcesso << std::endl
+		<< "Politica de substituicao (0 - LRU , 1 - aleatoria) " << politicaSubstituicao << std::endl
+
+		<< "Total de endereços: " << verificacoes << ". " << enderecosEscrita << " de escrita. " << (verificacoes - enderecosEscrita) << " de leitura." << std::endl
+
+		<< "Total de escritas da memoria principal: " << escritasMemoria << std::endl
+		<< "Total de leituras da memoria principal: " << leiturasMemoria << std::endl
+
+		// Taxa de acerto (hit rate): especificar esta taxa por leitura, escrita e global (colocar ao lado a quantidade);
+
+		<< "Taxa de acerto (hit rate): " << double(acertos)/double(verificacoes) << std::endl
+
+		// Tempo médio de acesso da cache (em ns): utilizar a fórmula vista em aula;
+
+		//<< "Numero de acertos: " << acertos << std::endl
+		//<< "Numero de erros: " << erros << std::endl
+	;
 
 	return 0;
 }
